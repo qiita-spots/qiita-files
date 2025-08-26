39e5d77e70fd17c0a8e4518543b64a7afad58dce
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-08-26 11:32:02 +0200

    add classifiers for py3.9 and py3.10

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ classes = """
     Programming Language :: Python
     Programming Language :: Python :: 3.5
     Programming Language :: Python :: 3.6
+    Programming Language :: Python :: 3.9
+    Programming Language :: Python :: 3.10
     Programming Language :: Python :: Implementation :: CPython
     Operating System :: POSIX :: Linux
     Operating System :: MacOS :: MacOS X
